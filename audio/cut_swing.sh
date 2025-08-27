8ca0eb9a90243c039bac67ea90317281fcbdc0c6
H4RUTY <st22067hr@gm.ibaraki-ct.ac.jp>
2025-08-28 08:20:41 +0900

    Change dir structure

--- audio/cut_swing.sh
+++ audio/cut_swing.sh
@@ -4,10 +4,14 @@ set -e
 brew install ffmpeg
 
 # input.mp3をwav変換し8秒ごとにカット
-input="../audio/input.mp3"
+input="./input.mp3"
+[ ! -f "$input" ] && { echo "input.mp3 が見つかりません"; exit 1; }
+
+mkdir -p ../src/audio
+
 for i in {0..5}; do
   start=$((i * 8))
-  out=$(printf "../audio/swing%02d.wav" $((i+1)))
+  out=$(printf "../src/audio/swing%02d.wav" $((i+1)))
   ffmpeg -y -i "$input" -ss $start -t 8 "$out"
 done
 
